a79058c1cfeb6d6e36f9aec2d65d345dcbf86c84
jmrigali <jmrigali@ymail.com>
2017-05-30 13:54:02 -0600

    cookies complete

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
+const cookieP= require('cookie-parser');
+
+app.use(cookieP());
 
 app.get('/set', (req,res,next) => {
-  
+  res.cookie("foo","bar").send();
 });
 
 app.get('/read', (req,res,next) => {
+  res.send(req.cookies);
 
 });
 
 app.get('/clear', (req,res,next) => {
-
+  res.clearCookie('deleteMe').send();
 });
 
 app.listen('3000', () => {
